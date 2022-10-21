c9dc8b6383139b9b302f61ff1aaea0831911f7a9
GRYYV2 <115805703+GRYYV2@users.noreply.github.com>
2022-10-21 13:15:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']
-global.OwnerNumber = ['918101187835']
-global.ownertag = ['918101187835']
-global.BotName = "Miku"
-global.packname = "Miku bot"
-global.author = "By: Fantox"
-global.OwnerName = "Fantox"
+global.Owner = ['919755062790']
+global.OwnerNumber = ['919755062790']
+global.ownertag = ['919755062790']
+global.BotName = "nik"
+global.packname = "nik"
+global.author = "By: nik"
+global.OwnerName = "nik"
 global.BotSourceCode = "https://github.com/FantoX001/Miku-MD"
 global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"
 global.sessionName = "session"
